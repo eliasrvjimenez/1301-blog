9a1c457b7c1d3582ba9371d1b2fb04d5e25d7c6d
eliasrvjimenez <eliasrvjimenez@gmail.com>
2022-09-26 17:16:35 -0500

    navbar commit

---
+++ src/Navigation/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+ return(
+  <nav className="navbar">
+   <a className="navbar-brand" href="#">
+   1301 Blog
+  </a>
+  <div className="navbar">
+   <button className="navbar-item"
+    type="button">
+    Personal Narrative
+  </button>
+  </div>
+  
+    
+  </nav>
+ );
+};
+
+export default Navbar;
